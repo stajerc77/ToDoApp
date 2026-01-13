c6e659566606de3495c050c09842103083654701
stajerc77 <mihak.dev@gmail.com>
2026-01-13 16:08:53 +0100

    Create tables and provide DB dependency

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .database import engine, SessionLocal, Base
+from .models import Task
 
-app = FastAPI(title="Todo API", description="Portfolio project")
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="ToDoApp API", description="App to structure everyday tasks")
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
-async def root():
-    return {"message": "🎉 FastAPI Todo App Working!", "status": "ready"}
-
-@app.get("/health")
-async def health():
-    return {"status": "healthy"}
+async def root(db: Session = Depends(get_db)):
+    return {"message": "🎉 FastAPI ToDo App Working!", "status": "ready"}
